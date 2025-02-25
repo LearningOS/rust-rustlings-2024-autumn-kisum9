f7bfab5845a562f1172cb6819c9de7e3bb9d4054
kisum9 <kisumtan93@gmail.com>
2025-02-25 14:28:23 +0800

    Update strings exercises

--- exercises/strings/strings1.rs
+++ exercises/strings/strings1.rs
@@ -5,13 +5,23 @@
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
 
+// Solution 1
+// fn current_favorite_color() ->  &'static str {
+//     "blue"
+// }
+
+// Solution 2
+// fn current_favorite_color() -> String {
+//     "blue".to_string()
+// }
+
+// Solution 3
 fn current_favorite_color() -> String {
-    "blue"
+    String::from("blue")
 }
